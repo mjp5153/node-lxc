e46011c612869a3d8ee5353fcd2a60f933e61ddf
Andy Greene <andygreenegrass@gmail.com>
2016-09-01 14:32:06 -0400

    promisifying

---
+++ examples/example.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+const lxc = require('../index.js');
+
+const start = Date.now();
+var current = start;
+var last = start;
+console.log('BEGIN: 0ms');
+
+function sleep(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration);
+  });
+}
+
+lxc.list().then(containers => {
+  containers.forEach(container => {
+    console.log(`LS RESULTS: ${container}`);
+  });
+}, err => {
+  console.log(err);
+});
+
+//return;
+
+// lxc.clone('icarus', 'icarus-1').then(() => {
+//   console.log('CLONED IT!!!!!!');
+// }, err => {
+//   console.log(err);
+// });
+
+const containerName = 'node-test';
+
+console.log(`creating ${containerName}...`);
+lxc.createUbuntu(containerName).then(() => {
+  current = Date.now();
+  console.log(`CREATED: ${current - last} ms`);
+  console.log(`starting ${containerName}...`);
+  return lxc.start(containerName);
+}).then(() => {
+  last = current;
+  current = Date.now();
+  console.log(`STARTED: ${current - last} ms`);
+  console.log(`executing cmd in ${containerName}...`);
+  return lxc.attach(containerName, 'hostname');
+}).then(stdout => {
+  last = current;
+  current = Date.now();
+  console.log(`ATTACH HOSTNAME: ${stdout.trim()}, ${current - last} ms`);
+  console.log('sleeping for 2 seconds...');
+  return sleep(2000);
+}).then(() => {
+  console.log(`getting info for ${containerName}...`);
+  return lxc.info(containerName);
+}).then(info => {
+  last = current;
+  current = Date.now();
+  console.log(JSON.stringify(info, null, 2));
+  console.log(`INFO: ${current - last} ms`);
+  console.log(`installing lxc in ${containerName}...`);
+  return lxc.attach(containerName, 'apt-get update; apt-get install -y --no-install-recommends lxc');
+}).then(() => {
+  last = current;
+  current = Date.now();
+  console.log(`INSTALL LXC: ${current - last} ms`);
+  console.log(`stopping ${containerName}...`);
+  return lxc.stop(containerName);
+}).then(() => {
+  last = current;
+  current = Date.now();
+  console.log(`STOPPED: ${current - last} ms`);
+  console.log(`getting info for ${containerName}...`);
+  return lxc.info(containerName);
+}).then(info => {
+  last = current;
+  current = Date.now();
+  console.log(JSON.stringify(info, null, 2));
+  console.log(`INFO: ${current - last} ms`);
+  console.log(`executing cmd in ${containerName}...`);
+  return lxc.execute(containerName, 'hostname');
+}).then(stdout => {
+  last = current;
+  current = Date.now();
+  console.log(`EXECUTE HOSTNAME: ${stdout.trim()}, ${current - last} ms`);
+  console.log(`destroying ${containerName}...`);
+  return lxc.destroy(containerName);
+}).then(() => {
+  last = current;
+  current = Date.now();
+  console.log(`DESTROYED: ${current - last} ms`);
+  console.log();
+  console.log(`TOTAL TIME: ${current - start} ms`);
+}).catch(err => {
+  console.log(err);
+});
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 /*
-    Wrapper Library for LXC Commands
+Wrapper Library for LXC Commands
 */
 
 const exec = require('child_process').exec;
@@ -9,93 +9,87 @@ const os = require('os');
 var lxc = {};
 // Create Container: lxc-create -n <name> -t <template>
 //    - example: lxc-create -n ubuntu-container -t ubuntu
-lxc.create = function(name, template, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-create -n ' + name + ' -t ' + template;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback();
+lxc.create = function(name, template) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-create -n ${name} -t ${template}`;
+    exec(cmd, err => {
+      if (err) {return reject(err);}
+      return resolve();
     });
+  });
 };
 
 // save needing to specify 'ubuntu'
-lxc.createUbuntu = function(name, callback){
-    lxc.create(name, 'ubuntu', callback);
+lxc.createUbuntu = function(name) {
+  return lxc.create(name, 'ubuntu');
 };
 
 // Destroy Container: lxc-destroy -n <name>
-lxc.destroy = function(name, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-destroy -n ' + name;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback();
+lxc.destroy = function(name) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-destroy -n ${name}`;
+    exec(cmd, err => {
+      if (err) {return reject(err);}
+      return resolve();
     });
+  });
 };
 
 // Clone Container: lxc-clone -o <existing-name> -n <new-name>
-lxc.clone = function(existingName, newName, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-clone -o ' + existingName + ' -n ' + newName;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback();
+lxc.clone = function(existingName, newName) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-clone -o ${existingName} -n ${newName}`;
+    exec(cmd, err => {
+      if (err) {return reject(err);}
+      return resolve();
     });
+  });
 };
 
 // Start Container: lxc-start -d -n <name>
-lxc.start = function(name, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-start -d -n ' + name;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback();
+lxc.start = function(name) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-start -d -n ${name}`;
+    exec(cmd, err => {
+      if (err) {return reject(err);}
+      return resolve();
     });
+  });
 };
 
 // Stop Container: lxc-stop -n <name>
-lxc.stop = function(name, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-stop -n ' + name;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback();
+lxc.stop = function(name){
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-stop -n ${name}`;
+    exec(cmd, err => {
+      if (err) {return reject(err);}
+      return resolve();
     });
+  });
 };
 
 // Get info: lxc-info -n <name>
-lxc.info = function(name, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-info -n ' + name;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback(null, lxc._parseInfo(stdout));
+lxc.info = function(name) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-info -n ${name}`;
+    exec(cmd, (err, stdout) => {
+      if (err) {return reject(err);}
+      return resolve(lxc._parseInfo(stdout));
     });
+  });
 };
 
-lxc._parseInfo = function(rawInfo){
-    rawInfo = rawInfo.split(os.EOL);
-    var info = {};
-    for (var i in rawInfo){
-        //console.log(rawInfo[i]);
-        if(rawInfo[i]){
-            var temp = rawInfo[i].trim().split(':');
-            info[temp[0].trim()] = temp[1].trim();
-        }
+lxc._parseInfo = function(rawInfo) {
+  rawInfo = rawInfo.split(os.EOL);
+  var info = {};
+  for (var i in rawInfo) {
+    //console.log(rawInfo[i]);
+    if (rawInfo[i]) {
+      var temp = rawInfo[i].trim().split(':');
+      info[temp[0].trim()] = temp[1].trim();
     }
-    return info;
+  }
+  return info;
 };
 
 // Run Command in running container: lxc-attach -n <name> -- <command>
@@ -103,15 +97,14 @@ lxc._parseInfo = function(rawInfo){
 //    - example: lxc-attach -n icarus-1 -- uname -a
 //    - redirects will not work, so use bash -c 'command' if you need redirects
 //    - example: lxc-attach -n icarus-1 -- bash -c 'ps -ef | grep node'
-lxc.attach = function(name, command, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-attach -n ' + name + ' -- ' + command;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback(null, stdout);
+lxc.attach = function(name, command) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-attach -n ${name} -- ${command}`;
+    exec(cmd, (err, stdout) => {
+      if (err) {return reject(err);}
+      return resolve(stdout);
     });
+  });
 };
 
 // Run Command in stopped container: lxc-execute -n <name> -- <command>
@@ -122,39 +115,36 @@ lxc.attach = function(name, command, callback){
 //
 // *NOTE* This requires the installation of lxc on the container:
 //              apt-get install -y --no-install-recommends lxc
-lxc.execute = function(name, command, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-execute -n ' + name + ' -- ' + command;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback(null, stdout);
+lxc.execute = function(name, command) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-execute -n ${name} -- ${command}`;
+    exec(cmd, (err, stdout) => {
+      if (err) {return reject(err);}
+      return resolve(stdout);
     });
+  });
 };
 
 // Pass Resource: lxc-device -n <name> add <interface-name> <interface-name-in-container>
-lxc.passResource = function(name, ifaceName, newIfaceName, callback){
-    callback = callback || function() {};
-    var cmd = 'lxc-device -n ' + name + ' add ' + ifaceName + ' ' + newIfaceName;
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback();
+lxc.passResource = function(name, ifaceName, newIfaceName) {
+  return new Promise((resolve, reject) => {
+    var cmd = `lxc-device -n ${name} add ${ifaceName} ${newIfaceName}`;
+    exec(cmd, err => {
+      if (err) {return reject(err);}
+      return resolve();
     });
+  });
 };
 
 // List Containers: lxc-ls
-lxc.list = function(callback){
-    callback = callback || function() {};
+lxc.list = function() {
+  return new Promise((resolve, reject) => {
     var cmd = 'lxc-ls';
-    exec(cmd, function(error, stdout, stderr){
-        if(error){
-            callback(error, stdout, stderr);
-        }
-        callback(null, stdout.split(os.EOL));
+    exec(cmd, (err, stdout) => {
+      if (err) {return reject(err);}
+      return resolve(stdout.trim().split(os.EOL));
     });
+  });
 };
 
 module.exports = lxc;
